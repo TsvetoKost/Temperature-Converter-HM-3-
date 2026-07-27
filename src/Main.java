2692e79f747c5c0d7647344bb12e216056c4a2eb
Kostiantyn <tsvetkov.kostiantyn@stud.onu.edu.ua>
2026-07-27 17:55:04 +0300

    Create converter project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            System.out.println("Temperature Converter");
+            System.out.println("Version 1.0.");
+    }
+}
